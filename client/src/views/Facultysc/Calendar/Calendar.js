b4ae0781765627947430725b86df72a595307176
NoorusSahar22 <57786619+NoorusSahar22@users.noreply.github.com>
2020-12-23 11:20:41 +0500

    calendar error solved

--- client/src/views/Facultysc/Calendar/Calendar.js
+++ client/src/views/Facultysc/Calendar/Calendar.js
@@ -58,20 +58,7 @@ const EventCalendar = ({ event: { event }, getCurrentEvents }) => {
   if (event != null) {
     myEventList = event.event;
   }
-  //  const StyledButton = withStyles({
-  //   root: {
-  //     background: 'linear-gradient(45deg, #fafffa 30%, #fafffa 90%)',
-  //     borderRadius: 3,
-  //     border: 0,
-  //     color: 'black',
-  //     height: 48,
-  //     padding: '0 30px',
-  //     // boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-  //   },
-  //   label: {
-  //     textTransform: 'capitalize',
-  //   },
-  // })(Button);
+
 
   const classes = useStyles();
   return (
